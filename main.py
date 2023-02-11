889932dd2b4ee0a51136ca308640eac9294d3e60
Широков Игорь <shirokovigor@mail.ru>
2023-02-11 12:01:31 +0300

    Added some modules

--- main.py
+++ main.py
@@ -92,6 +92,7 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
